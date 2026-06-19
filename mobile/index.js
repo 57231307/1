179fc801db48031cbac555ef24d161a6d2fd4536
57231307 <57231307@users.noreply.github.com>
2026-06-19 07:41:42 +0000

    feat: 项目评估
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- mobile/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * 冰溪 ERP 移动端入口
- */
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
